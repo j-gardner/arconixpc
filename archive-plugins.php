f60a146397447f30415dd89d82d72bb478059b8f
John Gardner <john@arconixpc.com>
2013-01-26 00:14:43 -0500

    update plugin archive

--- archive-plugins.php
+++ archive-plugins.php
@@ -1,8 +1,14 @@
 <?php
-remove_action( 'genesis_before_post_content', 'genesis_post_info' );
-//remove_action( 'genesis_after_post_content', 'genesis_post_meta' );
+/**
+ * The Plugins post type archive template
+ */
 
+remove_action( 'genesis_before_post_content', 'genesis_post_info' );
+
+add_filter( 'pre_get_posts', 'arconix_plugin_archive_change_order' );
 add_filter( 'genesis_post_meta', 'arconix_plugin_archive_post_meta' );
+
+
 /**
  * Return a custom meta for the "Plugins" archive
  *
@@ -17,13 +23,14 @@ function arconix_plugin_archive_post_meta( $post_meta ){
 
     $details = get_wporg_custom_plugin_data( $slug );
 
+    if( ! $details ) return;
+
     $post_meta = 'Version: ' . $details->version . ' | Last Updated: ' . date( get_option( 'date_format' ), strtotime( $details->last_updated ) );
     $post_meta .= '<span class="arconix-plugins-ago">(' . ARCONIX_PLUGINS::ago( strtotime( $details->last_updated ) ) . ')</span> | Downloads: ' . $details->downloaded;
 
     return $post_meta;
 }
 
-add_filter( 'pre_get_posts', 'arconix_plugin_archive_change_order' );
 /**
  * Change the Sort Order on the "Plugins" archive
  *
