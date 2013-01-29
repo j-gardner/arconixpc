6ff9f1ff7d30e3b2c059499ca09bd43e7aa0eaa6
John Gardner <john@arconixpc.com>
2013-01-28 23:29:47 -0500

    load the std droid sans font

--- functions.php
+++ functions.php
@@ -88,7 +88,7 @@ function arconix_post_type_supports() {
 function arconix_load_google_fonts() {
     wp_enqueue_style(
     	'google-fonts',
-    	'http://fonts.googleapis.com/css?family=Droid+Sans:700|Droid+Serif',
+    	'http://fonts.googleapis.com/css?family=Droid+Sans|Droid+Serif',
     	false,
         CHILD_THEME_VERSION
     );
