a937744b72f24d26b6cd15944015450167392ee4
John Gardner <john@arconixpc.com>
2013-01-28 22:35:30 -0500

    Fix a bug in the custom plugin data

--- archive-plugins.php
+++ archive-plugins.php
@@ -21,12 +21,12 @@ function arconix_plugin_archive_post_meta( $post_meta ){
     $slug = get_post_meta( $post->ID, '_acpl_slug', true );
     if( ! $slug ) return '';
 
-    $details = get_wporg_custom_plugin_data( $slug );
+    $details = unserialize( ARCONIX_PLUGINS::get_wporg_custom_plugin_data( $slug ) );
 
     if( ! $details ) return;
 
     $post_meta = 'Version: ' . $details->version . ' | Last Updated: ' . date( get_option( 'date_format' ), strtotime( $details->last_updated ) );
-    $post_meta .= '<span class="arconix-plugins-ago">(' . ARCONIX_PLUGINS::ago( strtotime( $details->last_updated ) ) . ')</span> | Downloads: ' . $details->downloaded;
+    $post_meta .= ' <span class="arconix-plugins-ago">(' . ARCONIX_PLUGINS::ago( strtotime( $details->last_updated ) ) . ')</span> | Downloads: ' . $details->downloaded;
 
     return $post_meta;
 }
