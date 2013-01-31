1169d2b4723266a422e8c655c0f2adff04f65d9c
John Gardner <john@arconixpc.com>
2013-01-31 17:43:25 -0500

    updated function layout

--- functions.php
+++ functions.php
@@ -20,10 +20,17 @@ add_filter( 'genesis_comment_form_args', 'arconix_comment_form_args' );
 add_filter( 'arconix_button_shortcode_args', 'arconix_child_button_args' );
 add_filter( 'arconix_portfolio_post_type_args', 'arconix_child_portfolio_args' );
 
-add_theme_support( 'genesis-structural-wraps', array( 'header', 'inner', 'footer' ) ); // Add Structural Wraps
-remove_theme_support( 'genesis-menus' ); // Unregister primary/secondary navigation menus
+// Add Structural Wraps
+add_theme_support( 'genesis-structural-wraps', array( 'header', 'inner', 'footer' ) );
 
-add_image_size( 'arconix-thumb', 320, 200, TRUE ); // Add our own Image size for the portfolio
+// Unregister primary/secondary navigation menus
+remove_theme_support( 'genesis-menus' );
+
+// Add our own Image size for the portfolio
+add_image_size( 'arconix-thumb', 320, 200, TRUE ); 
+
+// Sets Content Width
+$content_width = apply_filters( 'content_width', 740, 740, 1140 );
 
 // Configure layout settings
 genesis_set_default_layout( 'content-sidebar' );
@@ -36,31 +43,28 @@ unregister_sidebar( 'sidebar-alt' ); // Remove Secondary sidebar
 
 // Add additional sidebar areas
 genesis_register_sidebar( array(
-    'id' => 'home-full',
-    'name' => __( 'Full Width Home', 'arconix' ),
-    'description' => __( 'This single area is the top widget area on the homepage.', 'arconix' )
+	'id' => 'home-full',
+	'name' => __( 'Full Width Home', 'arconix' ),
+	'description' => __( 'This single area is the top widget area on the homepage.', 'arconix' )
 ) );
 
 genesis_register_sidebar( array(
-    'id' => 'home-left',
-    'name' => __( 'Home-Left', 'arconix' ),
-    'description' => __( 'This is the left-featured section on the homepage.', 'arconix' )
+	'id' => 'home-left',
+	'name' => __( 'Home-Left', 'arconix' ),
+	'description' => __( 'This is the left-featured section on the homepage.', 'arconix' )
 ) );
 
 genesis_register_sidebar( array(
-    'id' => 'home-right',
-    'name' => __( 'Home-Right', 'arconix' ),
-    'description' => __( 'This is the right-featured section on the homepage.', 'arconix' )
+	'id' => 'home-right',
+	'name' => __( 'Home-Right', 'arconix' ),
+	'description' => __( 'This is the right-featured section on the homepage.', 'arconix' )
 ) );
 
 genesis_register_sidebar( array(
-    'id' => 'feature-block',
-    'name' => __( 'Feature-Footer', 'arconix' ),
-    'description' => __( 'This single area is below the home block.', 'arconix' )
+	'id' => 'feature-block',
+	'name' => __( 'Feature-Footer', 'arconix' ),
+	'description' => __( 'This single area is below the home block.', 'arconix' )
 ) );
-
-// Sets Content Width
-$content_width = apply_filters( 'content_width', 740, 740, 1140 );
 
 /**
  * Add post type support for genesis-specific options
@@ -68,16 +72,16 @@ $content_width = apply_filters( 'content_width', 740, 740, 1140 );
  * @since 3.0
  */
 function arconix_post_type_supports() {
-    $post_types = array( 'plugins', 'portfolio' );
-    $supports = array( 'genesis-seo', 'genesis-simple-sidebars', 'genesis-layouts' );
+	$post_types = array( 'plugins', 'portfolio' );
+	$supports = array( 'genesis-seo', 'genesis-simple-sidebars', 'genesis-layouts' );
 
-    foreach( $post_types as $post_type ) {
-        if( ! post_type_exists( $post_type ) ) continue;
+	foreach( $post_types as $post_type ) {
+		if( ! post_type_exists( $post_type ) ) continue;
 
-        foreach ( $supports as $support ) {
-            add_post_type_support( $post_type, $support ) ;
-        }
-    }
+		foreach ( $supports as $support ) {
+			add_post_type_support( $post_type, $support ) ;
+		}
+	}
 }
 
 /**
@@ -86,12 +90,12 @@ function arconix_post_type_supports() {
  * @since 3.0
  */
 function arconix_load_google_fonts() {
-    wp_enqueue_style(
-    	'google-fonts',
-    	'http://fonts.googleapis.com/css?family=Droid+Sans|Droid+Serif',
-    	false,
-        CHILD_THEME_VERSION
-    );
+	wp_enqueue_style(
+		'google-fonts',
+		'http://fonts.googleapis.com/css?family=Droid+Sans|Droid+Serif',
+		false,
+		CHILD_THEME_VERSION
+	);
 }
 
 /**
@@ -100,7 +104,7 @@ function arconix_load_google_fonts() {
  * @since 3.0
  */
 function arconix_add_viewport_meta_tag() {
-    echo '<meta name="viewport" content="width=device-width, initial-scale=1.0"/>';
+	echo '<meta name="viewport" content="width=device-width, initial-scale=1.0"/>';
 }
 
 /**
@@ -111,7 +115,7 @@ function arconix_add_viewport_meta_tag() {
  * @return string
  */
 function arconix_footer_backtotop_text( $backtotop ) {
-    return '[footer_copyright] <a href="http://arcnx.co/1" class="arconix-footer-site-link">' . CHILD_THEME_NAME . '</a>';
+	return '[footer_copyright] <a href="http://arcnx.co/1" class="arconix-footer-site-link">' . CHILD_THEME_NAME . '</a>';
 }
 
 /**
@@ -122,7 +126,7 @@ function arconix_footer_backtotop_text( $backtotop ) {
  * @return string
  */
 function arconix_post_info( $post_info ) {
-    return '[post_date] [post_comments] [post_edit]';
+	return '[post_date] [post_comments] [post_edit]';
 }
 
 /**
@@ -133,7 +137,7 @@ function arconix_post_info( $post_info ) {
  * @return string
  */
 function arconix_post_meta( $post_meta ) {
-    return '[post_categories before="Filed In: "] [post_tags before="Tagged: "]';
+	return '[post_categories before="Filed In: "] [post_tags before="Tagged: "]';
 }
 
 /**
@@ -144,7 +148,7 @@ function arconix_post_meta( $post_meta ) {
  * @return string
  */
 function arconix_footer_creds_text( $creds ) {
-    return 'Powered by [footer_wordpress_link] and <a href="http://studiopress.com/themes/genesis">Genesis</a> | Hosted by <a href="http://arcnx.co/ix">IX Webhosting</a> | [footer_loginout]';
+	return 'Powered by [footer_wordpress_link] and <a href="http://studiopress.com/themes/genesis">Genesis</a> | Hosted by <a href="http://arcnx.co/ix">IX Webhosting</a> | [footer_loginout]';
 }
 
 /**
@@ -155,8 +159,8 @@ function arconix_footer_creds_text( $creds ) {
  * @return array $args
  */
 function arconix_comment_form_args( $args ) {
-    $args['title_reply'] = 'Leave a Comment';
-    return $args;
+	$args['title_reply'] = 'Leave a Comment';
+	return $args;
 }
 
 /**
@@ -167,8 +171,8 @@ function arconix_comment_form_args( $args ) {
  * @return array $defaults
  */
 function arconix_child_button_args( $defaults ) {
-    $defaults['color'] = 'green';
-    return $defaults;
+	$defaults['color'] = 'green';
+	return $defaults;
 }
 
 /**
@@ -178,6 +182,6 @@ function arconix_child_button_args( $defaults ) {
  * @return boolean $args
  */
 function arconix_child_portfolio_args( $args ) {
-    $args['has_archive'] = true;
-    return $args;
+	$args['has_archive'] = true;
+	return $args;
 }
