2a75392cf30e71e94db7f563920d64f28e4159d0
John Gardner <john@arconixpc.com>
2013-01-30 23:13:42 -0500

    moved image after the post title

--- archive-portfolio.php
+++ archive-portfolio.php
@@ -4,7 +4,7 @@
  */
 add_filter( 'genesis_pre_get_option_site_layout', '__genesis_return_full_width_content' );
 
-add_action( 'genesis_before_post_title', 'arconix_portfolio_archive_image' );
+add_action( 'genesis_after_post_title', 'arconix_portfolio_archive_image' );
 
 remove_action( 'genesis_before_post_content', 'genesis_post_info' );
 remove_action( 'genesis_post_content', 'genesis_do_post_content' );
@@ -17,16 +17,16 @@ remove_action( 'genesis_after_post_content', 'genesis_post_meta' );
  * @since 3.0
  */
 function arconix_portfolio_archive_image() {
-    if( has_post_thumbnail() ) {
-        // Get the url to the 'large' image
-        $_portfolio_img_url = wp_get_attachment_image_src( get_post_thumbnail_id(), 'large' );
+	if( has_post_thumbnail() ) {
+		// Get the url to the 'large' image
+		$_portfolio_img_url = wp_get_attachment_image_src( get_post_thumbnail_id(), 'portfolio-large' );
 
-        echo '<div class="arconix-portfolio-featured-image">';
-        echo '<a href="' . esc_url( $_portfolio_img_url[0] ) . '" title="' . the_title_attribute( 'echo=0' ) . '">';
-        echo get_the_post_thumbnail( $thumbnail->ID, 'arconix-thumb' );
-        echo '</a>';
-        echo '</div>';
-    }
+		echo '<div class="arconix-portfolio-featured-image">';
+		echo '<a href="' . esc_url( $_portfolio_img_url[0] ) . '" title="' . the_title_attribute( 'echo=0' ) . '">';
+		echo get_the_post_thumbnail( $thumbnail->ID, 'arconix-thumb' );
+		echo '</a>';
+		echo '</div>';
+	}
 }
 
 genesis();
