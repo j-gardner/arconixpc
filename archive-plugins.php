04a2ea0724542abe2bca0009f5dfa97621cb8ae0
John Gardner <john@arconixpc.com>
2013-02-10 20:36:03 -0500

    Added archive titles

--- archive-plugins.php
+++ archive-plugins.php
@@ -6,6 +6,8 @@ add_filter( 'genesis_pre_get_option_site_layout', '__genesis_return_full_width_c
 add_filter( 'pre_get_posts', 'arconix_plugin_archive_change_order' );
 add_filter( 'genesis_post_meta', 'arconix_plugin_archive_post_meta' );
 
+add_action( 'genesis_before_loop', 'arconix_before_loop' );
+
 remove_action( 'genesis_before_post_content', 'genesis_post_info' );
 
 
@@ -15,19 +17,18 @@ remove_action( 'genesis_before_post_content', 'genesis_post_info' );
  * @global object $post
  * @param string $post_meta
  * @return string $post_meta
+ * @since 0.5
  */
 function arconix_plugin_archive_post_meta( $post_meta ){
     global $post;
     $slug = get_post_meta( $post->ID, '_acpl_slug', true );
-    if( ! $slug ) return '';
+    if( ! $slug ) return;
 
     $details = unserialize( ARCONIX_PLUGINS::get_wporg_custom_plugin_data( $slug ) );
 
     if( ! $details ) return;
 
     $post_meta = 'Version: ' . $details->version . ' | Downloads: ' . $details->downloaded;
-    /*$post_meta = 'Version: ' . $details->version . ' | Last Updated: ' . date( get_option( 'date_format' ), strtotime( $details->last_updated ) );
-    $post_meta .= ' <span class="arconix-plugins-ago">(' . ARCONIX_PLUGINS::ago( strtotime( $details->last_updated ) ) . ')</span> | Downloads: ' . $details->downloaded;*/
 
     return $post_meta;
 }
@@ -37,12 +38,22 @@ function arconix_plugin_archive_post_meta( $post_meta ){
  *
  * @param array $query
  * @return array $query
+ * @since 0.5
  */
 function arconix_plugin_archive_change_order( $query ) {
     $query->set( 'orderby', 'title' );
     $query->set( 'order', 'asc' );
 
     return $query;
+}
+
+/**
+ * Display the Post Type Archive Title
+ *
+ * @since 0.5
+ */
+function arconix_before_loop() {
+    echo '<h1 class="entry-title">WordPress Plugins</h1>';
 }
 
 genesis();
--- archive-portfolio.php
+++ archive-portfolio.php
@@ -4,25 +4,14 @@
  */
 add_filter( 'genesis_pre_get_option_site_layout', '__genesis_return_full_width_content' );
 add_filter( 'genesis_link_post_title', '__return_false' );
-add_filter( 'genesis_post_title_text', 'arconix_post_title_text' );
 
+add_action( 'genesis_before_loop', 'arconix_before_loop' );
 add_action( 'genesis_after_post_title', 'arconix_portfolio_archive_image' );
 
 remove_action( 'genesis_before_post_content', 'genesis_post_info' );
 remove_action( 'genesis_post_content', 'genesis_do_post_content' );
 remove_action( 'genesis_post_content', 'genesis_do_post_image' );
 remove_action( 'genesis_after_post_content', 'genesis_post_meta' );
-
-/**
- * Modify the Post title
- *
- * @since 3.0
- * @param  string $title 
- * @return string $title
- */
-function arconix_post_title_text( $title ) {
-    //$title = get_the_content() . '<span class="arconix-portfolio-title-link">'
-}
 
 /**
  * Add the featured image to the portfolio archive
@@ -33,14 +22,23 @@ function arconix_post_title_text( $title ) {
 function arconix_portfolio_archive_image() {
     if( ! has_post_thumbnail() ) return;
 
-        // Get the url to the 'large' image
-        $_portfolio_img_url = wp_get_attachment_image_src( get_post_thumbnail_id(), 'portfolio-large' );
+    // Get the url to the 'large' image
+    $_portfolio_img_url = wp_get_attachment_image_src( get_post_thumbnail_id(), 'portfolio-large' );
 
-        echo '<div class="arconix-portfolio-featured-image">';
-        echo '<a href="' . esc_url( $_portfolio_img_url[0] ) . '" title="' . the_title_attribute( 'echo=0' ) . '">';
-        echo get_the_post_thumbnail( $thumbnail->ID, 'arconix-thumb' );
-        echo '</a>';
-        echo '</div>';
+    echo '<div class="arconix-portfolio-featured-image">';
+    echo '<a href="' . esc_url( $_portfolio_img_url[0] ) . '" title="' . the_title_attribute( 'echo=0' ) . '">';
+    echo get_the_post_thumbnail( $thumbnail->ID, 'arconix-thumb' );
+    echo '</a>';
+    echo '</div>';
+}
+
+/**
+ * Display the Post Type Archive Title
+ *
+ * @since 0.5
+ */
+function arconix_before_loop() {
+    echo '<h1 class="entry-title">Portfolio</h1>';
 }
 
 genesis();
