65a9f99ff639bfcecab3961a944f6cb0f7ade03d
John Gardner <john@arconixpc.com>
2014-03-31 23:15:48 -0400

    update single page

--- single-docs.php
+++ single-docs.php
@@ -5,23 +5,8 @@
  * @since  3.1
  */
 
-remove_filter( 'genesis_post_info', 'arconix_post_info' );
-remove_filter( 'genesis_post_meta', 'arconix_post_meta' );
-
-//add_filter( 'genesis_post_meta', 'arconix_docs_meta' );
-
 remove_action( 'genesis_before_post_content', 'genesis_post_info' );
+remove_action( 'genesis_after_post_content', 'genesis_post_meta' );
 
-/**
- * Modify the Post Meta text
- *
- * @since 3.1
- * @param string $post_meta
- * @return string
- */
-function arconix_docs_meta( $post_meta ) {
-    $return = '<div class="modified-date">' . the_modified_date( $before ='Last updated:', $echo = false ) . '</div>';
 
-    return $return;
-}
 genesis();
